16c23898b8564442a7741e1bb2cc67be525924e0
dayuwater <timmy940408@gmail.com>
2020-06-27 11:29:26 +0800

    travis

--- lesson-3-Container/exercises/udacity-c3-frontend/src/test.ts
+++ lesson-3-Container/exercises/udacity-c3-frontend/src/test.ts
@@ -1,6 +1,6 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-// Test Travis ssss
+// Test Travis sssss
 
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
